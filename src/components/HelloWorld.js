c5bdaf3b568e9e5592fc95d04a0cc8314e2e33fa
mstop4 <mstop4@gmail.com>
2018-01-22 17:22:03 -0500

    added HelloWorld comp

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div className='hello-world'>
+      <h1>Hello World!</h1>
+      <p>Welcome to my world</p>
+    </div>
+  )
+} 
+
+export default HelloWorld
