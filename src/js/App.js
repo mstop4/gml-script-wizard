a7f51cd22dbc41ac45f7f8951d742603789bb840
mstop4 <mstop4@gmail.com>
2018-01-23 11:19:43 -0500

    refactored App

--- src/js/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import OutputBox from './components/OutputBox'
-
-render(
-    <OutputBox />,
-    document.getElementById('app')
-)
---
+++ src/js/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import OutputBox from './OutputBox'
+import ArgumentBox from './ArgumentBox'
+
+const App = () => {
+    return (
+      <div className='app'>
+        <OutputBox/>
+        <ArgumentBox/>
+      </div>
+    )
+  } 
+  
+  export default App
