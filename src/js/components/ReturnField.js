9308b180dd7f4210d545d4a2d1426877b72faf06
mstop4 <mstop4@gmail.com>
2018-01-23 22:55:21 -0500

    converted ReturnField to FSC

--- src/js/components/ReturnField.js
+++ src/js/components/ReturnField.js
@@ -1,22 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../../styles/fields.css'
 
-class ReturnField extends Component {
-
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className='return-field'>
-        <h2>Returns</h2>
-        <input type='text'
-               value={this.props.value}
-               onChange={this.props.onChange}></input>
-      </div>
-    )    
-  }
+const ReturnField = ({ value, onChange }) => {
+  return (
+    <div className='return-field'>
+      <h2>Returns</h2>
+      <input type='text'
+             value={value}
+             onChange={onChange}></input>
+    </div>
+  )    
 }
 
 export default ReturnField
