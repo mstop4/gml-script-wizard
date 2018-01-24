5e6923d602f72b1af70ea4611aaf75c5c623e8df
mstop4 <mstop4@gmail.com>
2018-01-23 22:52:26 -0500

    converted DescriptionField to FSC, updated OutputBox tests

--- test/components/OutputBox.spec.js
+++ test/components/OutputBox.spec.js
@@ -8,10 +8,10 @@ const wrapper = shallow(<OutputBox />)
 describe('OutputBox Component', () => {
 
   it('has a title', () => {
-    expect(wrapper.find('h2').text()).to.equal('Output')
+    expect(wrapper.find('h2').text()).to.equal('Script')
   })
 
-  it('renders text box', () => {
-    expect(wrapper.find('textarea')).to.have.length(1);
+  it('renders a well', () => {
+    expect(wrapper.find('Well')).to.have.length(1);
   })
 })
