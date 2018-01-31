3da3ae86fdb5b9d6a626fd718ae0be3581749c0c
mstop4 <mstop4@gmail.com>
2018-01-31 15:45:22 -0500

    set up env variable for dev and prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
@@ -6,5 +7,13 @@ module.exports = merge( common, {
 
   devServer: {
     contentBase: './dist'
-  }
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ]
 })
