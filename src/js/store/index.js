640bd661e21a0ae03d13294074c69c1f84e30909
mstop4 <mstop4@gmail.com>
2018-03-08 11:33:00 -0500

    disable redux devtools

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,8 +1,11 @@
 import { createStore } from 'redux'
 import rootReducer from '../reducers/index'
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// TODO: Figure out how to configure Redux devtools for production/development
 
+const store = createStore(
+  rootReducer/*,*/
+  /*window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
+)
+  
 export default store
